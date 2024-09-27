eaca228ef7decc5c920d418915260424744546ef
wlgns410 <wlgns410@gmail.com>
2024-09-27 23:44:41 +0900

    ADD : user 모듈

--- NBE/src/module/user.module.ts
+++ NBE/src/module/user.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/infrastructure/user/entity/user.entity';
+import { UserController } from '../interface/presentation/user/controller/user.controller';
+import { UserService } from '../domain/user/service/user.service';
+import { UserRepositorySymbol } from '../domain/user/interface/user.repository';
+import { UserRepositoryImpl } from '../infrastructure/user/repository/user.repository.impl';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [UserController],
+  providers: [UserService, { provide: UserRepositorySymbol, useClass: UserRepositoryImpl }],
+  exports: [UserService],
 })
 export class UserModule {}
