48901dffb1af228934fc4f7e4c734092616cf704
wlgns410 <wlgns410@gmail.com>
2024-09-28 15:48:18 +0900

    스웨거 작성

--- NBE/src/interface/presentation/user/controller/user.controller.ts
+++ NBE/src/interface/presentation/user/controller/user.controller.ts
@@ -5,7 +5,9 @@ import { FindUserByIdUseCase } from '../../../../application/user/use-case/find-
 import { UpdateUserUseCase } from '../../../../application/user/use-case/update-user.use-case';
 import { UserDomain } from '../../../../domain/user/model/user.domain';
 import { Nullable } from '../../../../common/type/native';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UserController {
   constructor(
@@ -16,18 +18,25 @@ export class UserController {
   ) {}
 
   // 사용자 조회
+  @ApiOperation({ summary: '사용자 조회' }) // API 설명 추가
+  @ApiResponse({ status: 200, description: '사용자 정보 조회 성공' })
+  @ApiBearerAuth()
   @Get(':id')
   async getUserById(@Param('id') id: number): Promise<Nullable<UserDomain>> {
     return await this.findUserByIdUseCase.execute(id);
   }
 
   // 회원가입
+  @ApiOperation({ summary: '회원가입' }) // API 설명 추가
+  @ApiResponse({ status: 201, description: '회원가입 성공' })
   @Post('register')
   async registerUser(@Body() userData: UserDomain): Promise<UserDomain> {
     return await this.registerUserUseCase.execute(userData);
   }
 
   // 로그인
+  @ApiOperation({ summary: '로그인' }) // API 설명 추가
+  @ApiResponse({ status: 200, description: '로그인 성공' })
   @Post('login')
   async loginUser(@Body('email') email: string, @Body('password') password: string): Promise<{ token: string | null }> {
     const token = await this.loginUserUseCase.execute(email, password);
@@ -35,6 +44,9 @@ export class UserController {
   }
 
   // 사용자 정보 업데이트
+  @ApiOperation({ summary: '사용자 정보 업데이트' }) // API 설명 추가
+  @ApiResponse({ status: 200, description: '사용자 정보 업데이트 성공' })
+  @ApiBearerAuth()
   @Patch(':id')
   async updateUser(@Param('id') id: number, @Body() userData: UserDomain): Promise<UserDomain> {
     return await this.updateUserUseCase.execute(userData);
--- NBE/src/main.ts
+++ NBE/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './module/app.module';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.startAllMicroservices();
+  // Swagger 설정 로드
+  const swaggerDocument = JSON.parse(readFileSync(join(__dirname, '../swagger.json'), 'utf8'));
+
+  // Swagger 적용
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 
