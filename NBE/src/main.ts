81fd66b54a2c792c3bb675576dd70d3b76d9be5f
wlgns410 <wlgns410@gmail.com>
2024-10-10 21:31:55 +0900

    FEATURE : socket 연결

--- NBE/src/main.ts
+++ NBE/src/main.ts
@@ -3,15 +3,19 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './module/app.module';
 import { readFileSync } from 'fs';
 import { join } from 'path';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useWebSocketAdapter(new IoAdapter(app));
 
   // Swagger 설정 로드 (루트 경로에서 swagger.json을 읽도록 수정)
   const swaggerDocument = JSON.parse(readFileSync(join(__dirname, '../../swagger.json'), 'utf8'));
 
   SwaggerModule.setup('api', app, swaggerDocument);
 
+  // HTTP 서버 포트 설정 (같은 포트에서 HTTP + WebSocket 사용)
   await app.listen(3000);
 }
 
--- NBE/src/module/chat.module.ts
+++ NBE/src/module/chat.module.ts
@@ -5,11 +5,13 @@ import { ChatController } from '../interface/presentation/chat/controller/chat.c
 import { ChatService } from '../domain/chat/service/chat.service';
 import { ChatRepositorySymbol } from '../domain/chat/interface/chat.repository';
 import { ChatRepositoryImpl } from '../infrastructure/chat/repository/chat.repository.impl';
+import { ChatGateway } from '../infrastructure/socket/chat/chat-socket'; // ChatGateway를 가져옵니다
 
 @Module({
   imports: [TypeOrmModule.forFeature([ChatEntity])],
   controllers: [ChatController],
   providers: [
+    ChatGateway,
     ChatService,
     {
       provide: ChatRepositorySymbol,
