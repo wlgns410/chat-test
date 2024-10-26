12d8ba983231d4e081c5de74b8d6c8ce77445bb1
wlgns410 <wlgns410@gmail.com>
2024-10-26 16:37:43 +0900

    ADD : 스웨거 추가

--- NBE/src/interface/presentation/broadcast/controller/broadcast.controller.ts
+++ NBE/src/interface/presentation/broadcast/controller/broadcast.controller.ts
@@ -1,12 +1,19 @@
 import { BroadcastService } from '../../../../domain/broadcast/service/broadcast.service';
 import { BroadcastDomain } from '../../../../domain/broadcast/model/broadcast.domain';
-import { Controller, Get, Post, Put, Delete, Param, Body, HttpStatus, HttpException, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, HttpStatus, HttpException, Patch, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '../../../../common/guard/auth.guard';
 
+@ApiTags('chats')
 @Controller('broadcast')
 export class BroadcastController {
   constructor(private readonly broadcastService: BroadcastService) {}
 
   // 방송 생성
+  @ApiOperation({ summary: '방송 생성' })
+  @ApiResponse({ status: 200, description: '방송 생성' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @Post()
   async createBroadcast(@Body() broadcastData: BroadcastDomain): Promise<BroadcastDomain> {
     try {
@@ -17,6 +24,8 @@ export class BroadcastController {
   }
 
   // 특정 방송 조회
+  @ApiOperation({ summary: '특정 방송 조회' })
+  @ApiResponse({ status: 200, description: '특정 방송 조회' })
   @Get(':id')
   async getBroadcastById(@Param('id') id: number): Promise<BroadcastDomain | null> {
     try {
@@ -31,6 +40,10 @@ export class BroadcastController {
   }
 
   // 방송 업데이트
+  @ApiOperation({ summary: '방송 업데이트' })
+  @ApiResponse({ status: 200, description: '방송 업데이트' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @Put(':id')
   async updateBroadcast(@Param('id') id: number, @Body() broadcastData: BroadcastDomain): Promise<BroadcastDomain> {
     try {
@@ -42,6 +55,10 @@ export class BroadcastController {
   }
 
   // 방송 삭제
+  @ApiOperation({ summary: '방송 삭제' })
+  @ApiResponse({ status: 200, description: '방송 삭제' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async deleteBroadcast(@Param('id') id: number): Promise<void> {
     try {
@@ -52,6 +69,10 @@ export class BroadcastController {
   }
 
   // 방송 시작
+  @ApiOperation({ summary: '방송 시작' })
+  @ApiResponse({ status: 200, description: '방송 시작' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @Patch(':id/start')
   async startBroadcast(@Param('id') id: number, @Body() broadcastData: BroadcastDomain): Promise<BroadcastDomain> {
     try {
@@ -63,6 +84,10 @@ export class BroadcastController {
   }
 
   // 방송 종료
+  @ApiOperation({ summary: '방송 종료' })
+  @ApiResponse({ status: 200, description: '방송 종료' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @Patch(':id/end')
   async endBroadcast(@Param('id') id: number): Promise<void> {
     try {
